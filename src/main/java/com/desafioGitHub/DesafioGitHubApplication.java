ef2bff3d99925bb92064389df6ef14210ef98f58
ext_waguerom <ext_waguerom@falabella.cl>
2019-06-12 16:08:52 -0400

    JQ se da inicio al proyecto de desafio GitHub

---
+++ src/main/java/com/desafioGitHub/DesafioGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.desafioGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGitHubApplication.class, args);
+	}
+
+}
